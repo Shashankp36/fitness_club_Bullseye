f729e625331010c0f7c567500e55a1f074979469
Shashankp36 <shashankpatidar1@gmail.com>
2023-02-11 12:36:15 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+// App.js
+import React, { useState } from "react";
+import BookForm from "./components/bookForm";
+import BookList from "./components/bookList";
+import "./App.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-function App() {
+const App = () => {
+  const [books, setBooks] = useState([]);
+
+  const addBook = (book) => {
+    setBooks([...books, book]);
+  };
+
+  const removeBook = (index) => {
+    setBooks(books.filter((book, i) => i !== index));
+  };
+
+  const removeAll = () => {
+    setBooks([]);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1 className="text-center my-5">Book Management</h1>
+      <BookForm addBook={addBook} />
+      <BookList books={books} removeBook={removeBook} removeAll={removeAll} />
     </div>
   );
-}
+};
 
 export default App;
+
+
+
+
